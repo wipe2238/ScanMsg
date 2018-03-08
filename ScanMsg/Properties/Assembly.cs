37820f553758cd77cd76681e89507ec8158e8bf0
Wipe <wipe.git@gmail.com>
2018-03-08 03:36:11 +0100

    cleanup harder

---
+++ ScanMsg/Properties/Assembly.cs
@@ -0,0 +1,12 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle( "ScanMsg" )]
+[assembly: AssemblyProduct( "ScanMsg" )]
+[assembly: AssemblyDescription( "Fallout .msg files scanner" )]
+[assembly: AssemblyCompany( "Rotators" )]
+[assembly: AssemblyCopyright( "Copyright © Wipe 2018" )]
+[assembly: ComVisible( false )]
+[assembly: Guid( "af52633c-e8fb-4a78-9398-309979e84ca0" )]
+[assembly: AssemblyVersion( "0.3" )]
+[assembly: AssemblyFileVersion( "0.3" )]
